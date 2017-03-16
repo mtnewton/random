4f4a1885ffd939c026ee93b90635b1cd3c17db6b
Marvin Newton <mtnewton3@gmail.com>
2017-03-15 21:40:09 -0400

    [illusion-diamond] initial creation

---
+++ projects/illusion-diamond/js/main.js
@@ -0,0 +1,29 @@
+var segmentCount = 50
+var canvasSize = 600
+
+function setup() {
+    createCanvas(canvasSize, canvasSize)
+    noLoop()
+}
+
+function draw() {
+    clear()
+    var halfSize = canvasSize / 2
+    var segmentLength = halfSize / segmentCount
+    line(halfSize, 0, halfSize, canvasSize)
+    line(0, halfSize, canvasSize, halfSize)
+    makeSection(halfSize, 0, halfSize+segmentLength, halfSize, 0, segmentLength, segmentLength, 0)
+    makeSection(halfSize, 0, halfSize-segmentLength, halfSize, 0, segmentLength, -segmentLength, 0)
+    makeSection(halfSize, canvasSize, halfSize+segmentLength, halfSize, 0, -segmentLength, segmentLength, 0)
+    makeSection(halfSize, canvasSize, halfSize-segmentLength, halfSize, 0, -segmentLength, -segmentLength, 0)
+}
+
+function makeSection(x1, y1, x2, y2, dx1, dy1, dx2, dy2) {
+    for (var i = 0; i < segmentCount; i++) {
+        line(x1, y1, x2, y2)
+        x1 += dx1
+        y1 += dy1
+        x2 += dx2
+        y2 += dy2
+    }
+}
